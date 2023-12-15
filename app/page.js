877ed863b72b4f1af87e920ab9c7babff89490ce
Valeska Silva <valeska@wuhk.app>
2023-12-15 12:02:21 -0400

    Prueba

--- app/page.js
+++ app/page.js
@@ -1,113 +1,78 @@
+"use client"
 import Image from 'next/image'
+import './fonts.css';
+import { ReactLenis, useLenis } from '@studio-freight/react-lenis'
+import { Parallax } from '@/components/Parallax';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col">
+      <ReactLenis root options={{lerp:0.1}}>
+      <div className='h-screen relative'>
+  <img className="grafitti1" src="./images/grafitti1.png"/>
+  <img className="arrow" src="./images/arrow-down.png"/>
+ <img src="./logo.png" alt="La Cocina Pickleball Logo" className="w-1/2 md:w-auto lg:w-auto max-w-sm md:max-w-full lg:max-w-full h-auto absolute top-5 left-5" />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+ <div className="flex flex-row w-10/12 h-2/3 absolute top-48 right-0 justify-between">
+<div className='home-element'>
+  <h2 className='event'>Events</h2>
+  <div className='image-home'>
+  <img className="image-no-hover"src='./images/1.jpg'/>
+  <img className="image-hover" src='./images/2.jpg'/>
+  <img  className="grafitti2" src='./images/grafitti2.png'/>
+  </div>
+  
+</div>
+<div className='home-element middle-element'>
+  <h2 className='event'>Catalogo</h2>
+  <div className='image-home'>
+  <img className="image-no-hover"src='./images/4.jpg'/>
+  <img className="image-hover" src='./images/3.jpg'/>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  </div>
+  
+</div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+<div className='home-element last-element'>
+  <h2 className='event'>Sobre Nosotros</h2>
+  <div className='image-home'>
+  <img className="image-no-hover"src='./images/4.jpg'/>
+  <img className="image-hover" src='./images/3.jpg'/>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+  </div>
+  
+</div>
+</div>
+ <h1 className="text-sm md:text-base lg:text-lg text-gray-400 absolute bottom-0 left-5">LA CASA DEL PICKLEBALL EN ESPAÑOL</h1>
+ </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+ <div className='min-h-screen relative'>
+<div className='flex flex-row w-9/12 h-2/3 absolute top-38 right-0 justify-between pr-5'>
+  <div className="w-11/12 object-cover ">
+<Parallax speed={1} className="self-start object-cover">
+<img src='./images/5.jpg' className='object-cover min-w-full h-[70vh]' />
+</Parallax>
+</div>
+<Parallax speed={-2} className="self-end ">
+<p className='uppercase text-gray-400 ml-14 mt-32 text-sm md:text-base lg:text-lg '>
+La casa del pickleball en español que busca promover el deporte a todas las comunidades de habla hispana en el mundo, enfocándonos primordialmente en Latinoamérica.
+Llegó el momento de marcar la diferencia en el mundo de este emocionante deporte y llevar nuestras comunidades deportivas al siguiente nivel. Un deporte que une diversión y actividad física para todas las edades. Fácil de aprender y perfecto para construir conexiones.
+</p>
+</Parallax>
+</div>
+
+  </div>
+
+  <div className='contacto'>
+    <h1>¿Quieres llevar el Pickleball a tu país? Seremos tus mejores aliados.</h1>
+<button>Contactanos</button>
+  </div>
+
+
+    </ReactLenis>
+
+
+
     </main>
   )
 }
