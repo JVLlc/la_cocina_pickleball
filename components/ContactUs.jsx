3110f5995e795b7a60b7a969be648f6b7138ff17
juanchete <juanlopezlmg@gmail.com>
2024-01-09 17:41:00 -0400

    feat: Add react-toastify for displaying notifications

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -1,19 +1,18 @@
 import Footer from "./Footer";
 
 
-export function ContactUs({color,zIndex}) {
+import Link from "next/link";
 
-
-
-
+export function ContactUs({ color, zIndex }) {
   return (
-    <div className={`contacto ${color}`} >
-    <h1>
-      ¿Quieres llevar el Pickleball a tu país? Seremos tus mejores
-      aliados.
-    </h1>
-    <button>Contactanos</button>
-    <Footer/>
-  </div>
+    <div className={`contacto ${color}`}>
+      <h1>
+        ¿Quieres llevar el Pickleball a tu país? Seremos tus mejores aliados.
+      </h1>
+      <Link href="/contacto">
+          <button>Contactanos</button>
+      </Link>
+      <Footer />
+    </div>
   );
 }
